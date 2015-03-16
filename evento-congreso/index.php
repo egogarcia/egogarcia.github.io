b7d36901abf78d662bdce9cde646bbe582f57436
Didac Chaves <didacchaves@me.com>
2015-03-16 13:51:54 +0100

    modificar icones

--- evento-congreso/index.php
+++ evento-congreso/index.php
@@ -6,8 +6,8 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no"  />
         <meta http-equiv="X-UA-Compatible" content="IE=edge"  />
-        <meta name="description" content="Personaliza tu evento, congreso o conferencia con una aplicación nativa para Android y iOS, adaptamos y diseñamos la app a medida. Innova añadiendo interacción entre ponentes y público a través de su teléfono móvil"  />
-        <meta name="keywords" content="Aplicación, app, ios, android, congreso, evento, conferencia, interaccion, smartConf, smartConference, ponentes, publico, 2.0, interactivo"  />
+        <meta name="description" content="Personaliza tu evento, congreso o conferencia con una aplicación nativa para Android y iOS, adaptamos y diseñamos la app a medida. Innova añadiendo interacción."  />
+        <meta name="keywords" content="Aplicación, app, ios, android, congreso, evento, conferencia, interaccion, smartConf, smartConference, ponentes, publico, 2.0, interactivo, nativa"  />
 
         <meta name="apple-mobile-web-app-title" content="SmatConf."  />
         <meta name="apple-mobile-web-app-capable" content="yes"  />
@@ -21,9 +21,9 @@
         <meta property="og:site_name" content="SmatConf.">
         <meta property="og:image" content="http://getsmartconf.com/assets/img/iphone-1@2x.png" />
         <meta property="og:locale" content="es_ES">
-        
+
         <meta name="apple-itunes-app" content="app-id=961971666"/>
-        
+
         <meta itemprop="name" content="SmartConf.">
         <meta itemprop="description" content="Personaliza tu evento, congreso o conferencia con una aplicación nativa para Android y iOS, adaptamos y diseñamos la app a medida. Innova añadiendo interacción entre ponentes y público a través de su teléfono móvil">
         <meta itemprop="image" content="http://getsmartconf.com/assets/img/iphone-1@2x.png">
@@ -31,10 +31,10 @@
         <link rel="publisher" href="https://plus.google.com/107839398563835088830">
         <link rel="canonical" href="http://getsmartconf.com/">
         <meta name="google-site-verification" content="sg>TJbPRDKLbcKBvhuF-V9_SPK06EdR_fkk2qAsrVfZA" />
-        
-        
-        
-        
+
+
+
+
         <title>SmartConf. - App iOS y Android para eventos, congresos y conferencias</title>
 
         <!-- CSS -->
@@ -58,60 +58,74 @@
         <![endif]-->
 
         <!-- Favicon and touch icons -->
-        <link rel="shortcut icon" href="assets/ico/favicon.png">
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/144x144.png">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/114x114.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/72x72.png">
-        <link rel="apple-touch-icon-precomposed" href="assets/ico/57x57.png">
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" sizes="57x57" href="assets/ico/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="assets/ico/apple-touch-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="assets/ico/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="assets/ico/apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="assets/ico/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="assets/ico/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="assets/ico/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="assets/ico/apple-touch-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/ico/apple-touch-icon-180x180.png">
+        <link rel="icon" type="image/png" href="assets/ico/favicon-16x16.png" sizes="16x16">
+        <link rel="icon" type="image/png" href="assets/ico/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="assets/ico/favicon-96x96.png" sizes="96x96">
+        <link rel="icon" type="image/png" href="assets/ico/android-chrome-192x192.png" sizes="192x192">
+        <meta name="msapplication-square70x70logo" content="assets/ico/smalltile.png" />
+        <meta name="msapplication-square150x150logo" content="assets/ico/mediumtile.png" />
+        <meta name="msapplication-wide310x150logo" content="assets/ico/widetile.png" />
+        <meta name="msapplication-square310x310logo" content="assets/ico/largetile.png" />
 
     </head>
 
     <body>
         <script type="text/javascript" src="assets/js/analyticstracking.js"></script>
         <!-- Top menu -->
-        <?php 
-        $index = true;
-        include 'blocks/menu.php';
-        ?>
+        <header>
+            <?php
+            $index = true;
+            include 'blocks/menu.php';
+            ?>
 
-        <!-- Top content -->
-        <div class="top-content">
-            <div class="inner-bg">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-5 phone wow fadeInLeft">
-                            <img src="assets/img/iphone-1.png" alt="" title="pantalla inicial App">
-                        </div>
-                        <div class="col-sm-7 text wow fadeInUp">
-                            <h1>Impulsa tu <strong>evento</strong> con tu propia <strong>aplicación nativa</strong></h1>
-                            <div class="description">
-                                Nunca más podrás <strong>organizar el evento</strong>, conferencia o congreso sin la aplicación <strong>SmartConf.</strong><br/> 
-                                <strong>Fideliza asistentes</strong> interactuando entre ponentes y público.
-                                <br/><br/>¡Descargate la demo!
+            <!-- Top content -->
+            <div class="top-content">
+                <div class="inner-bg">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-5 phone wow fadeInLeft">
+                                <img src="assets/img/iphone-1.png" alt="Imagen de la pantalla inicial de la App" title="pantalla inicial App">
                             </div>
-                            <div class="top-button">
-                                <a class="button-1" href="https://appsto.re/es/sxOv5.i" title="Descargar en App Store">
-                                    <span class="button-1-left"><i class="fa fa-mobile"></i></span>
-                                    <span class="button-1-right">
-                                        <span class="button-1-right-top">Disponible en la</span>
-                                        <span class="button-1-right-bottom">App Store</span>
-                                    </span>
-                                </a>
+                            <div class="col-sm-7 text wow fadeInUp">
+                                <h1>Impulsa tu <strong>evento</strong> con tu propia <strong>aplicación nativa</strong></h1>
+                                <div class="description">
+                                    Nunca más podrás <strong>organizar el evento</strong>, conferencia o congreso sin la aplicación <strong>SmartConf.</strong><br/> 
+                                    <strong>Fideliza asistentes</strong> interactuando entre ponentes y público.
+                                    <br/><br/>¡Descargate la demo!
+                                </div>
+                                <div class="top-button">
+                                    <a class="button-1" href="https://appsto.re/es/sxOv5.i" title="Descargar en App Store">
+                                        <span class="button-1-left"><i class="fa fa-mobile"></i></span>
+                                        <span class="button-1-right">
+                                            <span class="button-1-right-top">Disponible en la</span>
+                                            <span class="button-1-right-bottom">App Store</span>
+                                        </span>
+                                    </a>
 
-                                <a class="button-2" href="https://play.google.com/store/apps/details?id=io.webyo.SmartConf" title="Descargar en Play Store, Android">
-                                    <span class="button-2-left"><i class="fa fa-android"></i></span>
-                                    <span class="button-2-right">
-                                        <span class="button-2-right-top">Descargar para</span>
-                                        <span class="button-2-right-bottom">Android</span>
-                                    </span>
-                                </a>
+                                    <a class="button-2" href="https://play.google.com/store/apps/details?id=io.webyo.SmartConf" title="Descargar en Play Store, Android">
+                                        <span class="button-2-left"><i class="fa fa-android"></i></span>
+                                        <span class="button-2-right">
+                                            <span class="button-2-right-top">Descargar para</span>
+                                            <span class="button-2-right-bottom">Android</span>
+                                        </span>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-
+        </header>
         <!-- Features -->
         <div class="features-container section-container" id="features">
             <div class="container">
@@ -148,7 +162,7 @@
                         </div>
                     </div>
                     <div class="col-sm-4 features-center wow fadeInDown">
-                        <img src="assets/img/iphone-3.png" alt="" title="pantalla inicial app">
+                        <img src="assets/img/iphone-3.png" alt="Imagen de la pantalla del eschedule de al App" title="pantalla inicial app">
                     </div>
                     <div class="col-sm-4 features-right wow fadeInUp">
                         <div class="features-box">
@@ -176,7 +190,7 @@
                 </div>
             </div>
         </div>
-        <?php include 'blocks/footer.php';?>
+        <?php include 'blocks/footer.php'; ?>
         <!-- Javascript -->
         <script src="assets/js/jquery-1.11.1.min.js"></script>
         <script src="assets/bootstrap/js/bootstrap.min.js"></script>
